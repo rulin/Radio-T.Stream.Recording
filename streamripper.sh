2e3aacd582eb14776abb74572948cbc428860007
rulin <rulin8@gmail.com>
2011-08-03 01:17:29 +0400

    fix script for reconect if transmitting server not started

--- streamripper.sh
+++ streamripper.sh
@@ -6,7 +6,23 @@ dirPath=`pwd`"/radio-t/"${strDate}
 #fileName="radio-t."${strTime}".mp3"
 #fullPath=${dirPath}"/"${fileName}
 let "lengthSecond=60*60*12"
+timestart=`date +%s`
+let "endtime=timestart+lengthSecond"
+countProcess=1
+streamRipperPath="/usr/bin/streamripper"
 
 mkdir -p ${dirPath}
-/usr/bin/streamripper ${streamURL} -a -A -s -d ${dirPath} -l ${lengthSecond} --quiet &
+while [ 1 == 1 ]; do
+        currenttime=`date +%s`
+        if [[ "$currenttime" -ge "$endtime" ]]
+        then
+                break
+        fi
 
+        currentCountProcess=`ps ax | egrep '[0-9]+:[0-9]+ ${streamRipperPath}' | wc -l`
+        if [[ "$currentCountProcess" -lt "$countProcess" ]]
+        then
+                ${streamRipperPath} ${streamURL} -a -A -s -d ${dirPath} -l ${lengthSecond} --quiet &
+        fi
+        sleep 10
+done
